79a7251c0ef37a035d12531709692a2760ac2319
erlanbelekov <belekoverlanhehe@gmail.com>
2021-08-15 22:58:06 +0600

    define Drawer navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,11 +2,29 @@ import * as React from 'react';
 import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { darkTheme, lightTheme } from '../constants';
-import { AddSpending, HomeScreen } from '../screens';
+import { AddSpending, HomeScreen, SettingsScreen } from '../screens';
 
 const RootStack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator>
+    <Drawer.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{ headerShown: false }}
+    />
+    <Drawer.Screen
+      name="Settings"
+      component={SettingsScreen}
+      options={{ headerShown: false }}
+    />
+  </Drawer.Navigator>
+);
 
 export default function RootNavigationContainer() {
   const scheme = useColorScheme();
@@ -16,8 +34,8 @@ export default function RootNavigationContainer() {
       <RootStack.Navigator>
         <RootStack.Group>
           <RootStack.Screen
-            name="Home"
-            component={HomeScreen}
+            name="DrawerNavigator"
+            component={DrawerNavigator}
             options={{ headerShown: false }}
           />
         </RootStack.Group>
