6b3d64cd416c09228922c6233a409e13463eeb1f
erlanbelekov <belekoverlanhehe@gmail.com>
2021-08-18 00:08:04 +0600

    create Tag component

---
+++ src/ui/Tag/index.tsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from 'react';
+import { ViewStyle, PressableProps, Pressable } from 'react-native';
+import { Label } from '../Label';
+import { useTheme } from '../../hooks';
+import { ThemeColor } from '../../types';
+import { FontSizes, Spacing } from '../../constants';
+
+export interface TagProps extends PressableProps {
+  color?: ThemeColor;
+  styles?: ViewStyle;
+  text: string;
+}
+
+export const Tag = ({
+  color = 'accent',
+  styles = {},
+  text,
+  ...pressableProps
+}: TagProps) => {
+  const { colors } = useTheme();
+
+  const bgColor = useMemo<string>(() => colors[color], [colors, color]);
+
+  return (
+    <Pressable
+      style={{
+        backgroundColor: bgColor,
+        padding: Spacing.MD,
+        borderRadius: Spacing.SM,
+        ...styles,
+      }}
+      {...pressableProps}>
+      <Label fontSize={FontSizes.MD} color="white">
+        {text}
+      </Label>
+    </Pressable>
+  );
+};
